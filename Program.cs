6867193660f9b092b9d59866cd92b90c8a067019
Marcin Waw <marcin@MacBook-Pro.local>
2017-04-05 22:51:58 +0100

    initial version

--- Program.cs
+++ Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using DotNetApp.Model;
 
 namespace ConsoleApplication
 {
+public class Game
+{
+    public int _val;
+    public Game(int i)
+    {
+        this._val = i;
+    }
+    public string GetPosition(int i)
+    {
+        return "Position " + i.ToString();
+    }
+}
     public class Program
     {
+           public static byte[] GetBytesDouble( double argument )
+    {
+        byte[] byteArray = BitConverter.GetBytes( argument );
+
+        return byteArray;
+    }
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Car c = new SportCar();
+            
+            c.ShowCar();
+            c.Accelerate();
+            c.ShowCar();
+
+            Console.WriteLine("Hello World! ");
         }
     }
 }
